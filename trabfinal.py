22995351dd483f5ddf1eeb6ae852e4bec4928f66
Raphael Queiroz <rapha95@gmail.com>
2019-07-04 10:54:42 -0300

    Adicionado dicionário

--- trabfinal.py
+++ trabfinal.py
@@ -2,10 +2,26 @@ import config # python file with API keys
 import tweepy
 import json
 import pandas as pd
-from textblob import TextBlob
-from googletrans import Translator
 import emoji
 import re
+import csv
+import time
+
+from textblob import TextBlob
+
+# load positive words from dictionary
+positives = []
+positives_file = open('positivos.txt', 'r')
+for word_file in positives_file:
+  word_file = re.sub(r'\n', '', word_file.strip())
+  positives.append(word_file)
+
+# load negative words from dictionary
+negatives = []
+negatives_file = open('negativos.txt', 'r')
+for word_file in negatives_file:
+  word_file = re.sub(r'\n', '', word_file.strip())
+  negatives.append(word_file)
 
 def clean_tweet(tweet): 
   # remove ats
@@ -19,7 +35,29 @@ def clean_tweet(tweet):
   # remove extra ponctuation
   tweet = re.sub(r'\.{2,}', '.', tweet)
   tweet = re.sub(r'\?{2,}', '?', tweet)
-  tweet = re.sub(r'!{2,}', '!', tweet)
+  tweet = re.sub(r'\!{2,}', '!', tweet)
+  # remove ponctuation
+  tweet = re.sub(r'\*', ' ', tweet)
+  tweet = re.sub(r'\.', ' ', tweet)
+  tweet = re.sub(r'\,', ' ', tweet)
+  tweet = re.sub(r'\?', ' ', tweet)
+  tweet = re.sub(r'\!', ' ', tweet)
+  tweet = re.sub(r'\#', ' ', tweet)
+  tweet = re.sub(r'\$', ' ', tweet)
+  tweet = re.sub(r'\%', ' ', tweet)
+  tweet = re.sub(r'\(', ' ', tweet)
+  tweet = re.sub(r'\)', ' ', tweet)
+  tweet = re.sub(r'\:', ' ', tweet)
+  tweet = re.sub(r'\\', ' ', tweet)
+  tweet = re.sub(r'\/', ' ', tweet)
+  tweet = re.sub(r'\|', ' ', tweet)
+  tweet = re.sub(r'\[', ' ', tweet)
+  tweet = re.sub(r'\]', ' ', tweet)
+  tweet = re.sub(r'\{', ' ', tweet)
+  tweet = re.sub(r'\}', ' ', tweet)
+  tweet = re.sub(r'\-', ' ', tweet)
+  tweet = re.sub(r'\_', ' ', tweet)
+
   return tweet
 
 def processing_tweet(tweet):
@@ -31,22 +69,19 @@ def processing_tweet(tweet):
   tweet = re.sub(r'\bt[ô|o]\b', 'estou', tweet)
   return tweet
 
-def analyse_tweet(tweet_text):  
-  # translator = Translator()
-  # text_en = translator.translate(tweet_text, src='pt', dest='en')
-
-  text_pt = TextBlob(tweet_text)
-  # blob = TextBlob(str(text_pt.translate(from_lang='pt', to='en')))
+def analyse_tweet(tweet_text):
+  sentiment = 0
+  text = tweet_text.split(" ")
+  for word in text:
+    for w_file in positives:
+      if word == w_file:
+        if sentiment <= 0:
+          sentiment += 1
+    for w_file in negatives:
+      if word == w_file:
+        if sentiment >= 0:
+          sentiment -= 1
   
-  # blob = TextBlob(text_en.text)
-  polarity = text_pt.sentiment.polarity
-
-  if polarity > 0:
-    sentiment = 1
-  elif polarity < 0:
-    sentiment = -1
-  else:
-    sentiment = 0 
   return sentiment
 
 def get_tweets(keyword):
@@ -62,21 +97,47 @@ def get_tweets(keyword):
     for status in page:
       status = status._json
       tweet = status['full_text']
+      
       # ignore retweets
-      if tweet[0:2] == 'rt':
+      if (str(tweet[0:2]) == 'rt') or (str(tweet[0:2]) == 'RT'):
         continue
       tweet = clean_tweet(tweet)
       tweet_text = processing_tweet(tweet)
-      
+        
       sentiment = analyse_tweet(tweet_text)
       tweets.append({'tweet_id': status['id'], 'text': tweet_text, 'polarity': sentiment})
   
   return tweets
+
+def eval_tweets(csv_name):
+  sum_polarity = 0
+  with open(csv_name) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+      if row[0] != 'polarity':
+        sum_polarity += int(row[0],10)
+
+  return sum_polarity
 
 def construct_dataset(csv_name):
   tweets = get_tweets('unb')
   tweets_df = pd.DataFrame(tweets)
   tweets_df.drop_duplicates(subset='text', keep='first')
   tweets_df.to_csv(csv_name, encoding='utf-8', index=False)
+
+  polarity_total = eval_tweets(csv_name)
+  if polarity_total <= -15:
+    classification = 'Péssimo'
+  if -15 < polarity_total <= -5:
+    classification = 'Meio ruim'
+  if -5 < polarity_total <= 5:
+    classification = 'Neutro'
+  if 5 < polarity_total < 15:
+    classification = 'Meio bom'
+  if polarity_total >= 15:
+    classification = 'Ótimo'
+
+  print(polarity_total)
+  print(classification)
 
 construct_dataset('unb_tweets_processed.csv')
